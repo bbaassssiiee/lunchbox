2dd947e181e1972b9fd0b8157bd5e43eaf0bd2ea
Bas Meijer <bas.meijer@me.com>
2015-07-06 22:22:20 +0200

    install galaxy roles first

--- run_this_first.sh
+++ run_this_first.sh
@@ -2,9 +2,10 @@
 
 ## run this to download & install  dependencies for lunchbox
 
+# Install roles used from galaxy.ansible.com and/or github.com
+ansible-galaxy install --force --role-file requirements.yml
+
 # Install python extensions for Ansible
 pip install --upgrade -r requirements.pip
 
-# Install roles used from galaxy.ansible.com and/or github.com
-ansible-galaxy install --force --role-file requirements.yml
 
