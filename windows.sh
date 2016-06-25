68207f4eae0276bfdfeb46b1bb63391613851b8e
Bas Meijer <bas.meijer@me.com>
2016-06-25 09:14:19 +0200

    add a control host for windows users

--- windows.sh
+++ windows.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # Setup Ansible for Local Use under vagrant and Run
-cp /vagrant/vagrant.ini /etc/ansible/hosts -f
-chmod 666 /etc/ansible/hosts
+sudo cp -f /vagrant/inventory/hosts /etc/ansible/ 
+sudo chmod 666 /etc/ansible/hosts
 # make sure hostname is set on localhost
 # this is necesary for ansible to be able to run directly in the box
 sudo hostname localhost
-sudo ansible-playbook /vagrant/vagrant.yml --connection=local
+sudo ansible-playbook /home/vagrant/lunchbox/site.yml --connection=local
 # set this back to default
 #sudo hostname default
